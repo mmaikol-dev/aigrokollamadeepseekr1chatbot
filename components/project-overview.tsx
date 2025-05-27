a91fdb29fd3f3e907754c6dc7670f332a5e4edda
mmaikol-dev <itrealdeallogistic@gmail.com>
2025-05-27 18:17:06 +0300

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -2,15 +2,8 @@ import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Vercel x Groq Chatbot</h1>
-      <p className="text-center">
-        This starter project uses <Link href="https://groq.com/">Groq</Link>{" "}
-        with the <Link href="https://sdk.vercel.ai/docs">AI SDK</Link> via the{" "}
-        <Link href="https://vercel.com/marketplace/groq">
-          Vercel Marketplace
-        </Link>
-        .
-      </p>
+      <h1 className="text-3xl font-semibold mb-4">A T L A S </h1>
+     
     </div>
   );
 };
